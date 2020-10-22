cc0dcbd15c0a47881b78c2683950419af5974fab
Victor Hugo de Oliveira <victor-olliveira@live.com>
2020-10-21 23:53:16 -0300

    feat(*): add gem 'rack-cors' and allow all origins

--- dim0517-api/config/application.rb
+++ dim0517-api/config/application.rb
@@ -32,6 +32,17 @@ module Dim0517Api
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
 
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource(
+          '*',
+          headers: :any,
+          methods: [:get, :patch, :put, :delete, :post, :options]
+          )
+      end
+    end
+
     config.generators do |g|
       g.test_framework :rspec, fixture: true
       g.fixture_replacement :factory_bot, dir: 'spec/factories'
